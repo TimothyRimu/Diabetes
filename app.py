2b244db57b628524610313a4c9a2646a3b328f2a
TimothyRimu <timothymrimu@gmail.com>
2024-08-04 16:09:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import streamlit.components.v1 as components
-import matplotlib.pyplot as plt
 
 # import ML libraries
 from sklearn.ensemble import GradientBoostingClassifier
